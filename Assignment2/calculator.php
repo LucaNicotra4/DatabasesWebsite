31aa1d6d54a3e9b56d2eeef2a14f249264bf98e4
Luca Nicotra <Lsnicotra44@gmail.com>
2023-10-20 13:13:24 -0500

    Homework 2 completion

--- Assignment2/calculator.php
+++ Assignment2/calculator.php
@@ -1,59 +1,103 @@
 <?php
 
-     $num1 = $_POST["num1"];
-     $num2 = $_POST["num2"];
-     $operator = $_POST["operator"];
-     if(isset($num1)){
+$answer = $_POST['answer'];
+$fillFirst = $_POST['fillFirst'];
+$fillSecond = $_POST['fillSecond'];
+echo "Answer: $answer <br>";
+echo "First: $fillFirst <br>";
+echo "Second: $fillSecond <br>";
+
+     //Main input form
+     echo "<html style='text-align:center'>";
+     echo "
+          <form method=\"POST\"> 
+               <h1>PHP Calculator 2</h1>
+               <p id=\"test\">Enter two numbers and select an operator</p>
+               <div id=\"bodyWrap\">
+                    <label for=\"num1\">Num 1</label>";
+                    if($fillFirst){
+                         echo "<input id=\"num1\" name=\"num1\" type=\"text\" value=$answer>";
+                    }else{
+                         echo "<input id=\"num1\" name=\"num1\" type=\"text\">";
+                    }
+     echo"
+                    <br><br>
+
+                    <div>Operator: </div>
+                    <input type=\"radio\" id=\"+\" name=\"operator\" value=\"+\">
+                    <label for=\"+\">+</label><br>
+                    <input type=\"radio\" id=\"-\" name=\"operator\" value=\"-\">
+                    <label for=\"-\">-</label><br>
+                    <input type=\"radio\" id=\"x\" name=\"operator\" value=\"x\">
+                    <label for=\"x\">x</label><br>
+                    <input type=\"radio\" id=\"/\" name=\"operator\" value=\"/\">
+                    <label for=\"/\">/</label>
+                    <br><br>
+
+                    <label for=\"num2\">Num 2</label>";
+                    if($fillSecond){
+                         echo "<input id=\"num2\" name=\"num2\" type=\"text\" value=$answer>";
+                    }else{
+                         echo "<input id=\"num2\" name=\"num2\" type=\"text\">";
+                    }
+     echo "
+                    <br><br>
+                    
+                    <input type='hidden' name='answer' value=$answer>
+                    <input type='hidden' name='answer' value=$fillFirst>
+                    <input type='hidden' name='answer' value=$fillSecond>
+                    <input type=\"submit\" name=\"submit1\">
+               </div>
+          </form>
+     ";
+
+     //Handling first form
+     if($_POST['submit1']){
+          $num1= $_POST["num1"];
+          $num2= $_POST["num2"];
+          $operator = $_POST["operator"];
+          $answer = $_POST["answer"];
           echo $num1;
           echo " ".$operator;
           echo " ".$num2;
           ?><br><?php
-
           if($operator == "+"){
-               $sum = $num1 + $num2;
-               echo "Answer: ".$sum;
+               $answer = $num1 + $num2;
+               echo "Answer: ".$answer;
           }else if($operator == "-"){
-               $sum = $num1 - $num2;
-               echo "Answer: ".$sum;
+               $answer = $num1 - $num2;
+               echo "Answer: ".$answer;
           }else if($operator == "x"){
-               $product = $num1 * $num2; 
-               echo "Answer: ".$product;
+               $answer = $num1 * $num2; 
+               echo "Answer: ".$answer;
           }else if($operator == "/"){
-               $divisor = $num1 / $num2; 
-               echo "Answer: ".$divisor;
+               $answer = $num1 / $num2; 
+               echo "Answer: ".$answer;
           }else{
                echo "Invalid operator";
           }
-     }else{
-          
      }
-     
+
+     //Second form (first button)
+     if(isset($answer)) {
+          echo "
+               <form method='POST'>
+                    <input type='hidden' name='answer' value=$answer>
+                    <input type='hidden' name='fillFirst' value=$answer>
+                    <input type='submit' name='submit2' value='Add $answer to Box 1'>
+               </form>
+          ";
+     }
+
+     //Third form (second button)
+     if(isset($answer)) {
+          echo "
+          <form method='POST'>
+               <input type='hidden' name='answer' value=$answer>
+               <input type='hidden' name='fillSecond' value=$answer>
+               <input type='submit' name='submit3' value='Add $answer to Box 2'>
+          </form>
+          ";
+     }
+
 ?>
-
-<!-- action="calculator_action.php" -->
-<form method="POST"> 
-          <h1>PHP Calculator</h1>
-          <p id="test">Enter two numbers and select an operator</p>
-          <div id="bodyWrap">
-               <label for="num1">Num 1</label>
-               <input id="num1" name="num1" type="text">
-               <br><br>
-               
-               <div>Operator: </div>
-               <input type="radio" id="+" name="operator" value="+">
-               <label for="+">+</label><br>
-               <input type="radio" id="-" name="operator" value="-">
-               <label for="-">-</label><br>
-               <input type="radio" id="x" name="operator" value="x">
-               <label for="x">x</label><br>
-               <input type="radio" id="/" name="operator" value="/">
-               <label for="/">/</label>
-               <br><br>
-
-               <label for="num2">Num 2</label>
-               <input id="num2" name="num2" type="text">
-               <br><br>
-
-               <input type="submit" name="submit">
-          </div>
-</form>
